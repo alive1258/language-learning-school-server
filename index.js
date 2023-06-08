d59348cbda2065032a30e856c9626ad8817b7e3b
alive1258 <javashazib@gmail.com>
2023-06-08 21:58:30 +0600

    all information data lodded and display show on client side done

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1yov7jc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,28 +17,39 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const informationCollection = client
+      .db("languageSchoolDB")
+      .collection("informations");
+
+    app.get("/informations", async (req, res) => {
+      const result = await informationCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("School is Starting");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Language Learning School is coming on port ${port}`);
-  });
+  res.send("School is Starting");
+});
+
+app.listen(port, () => {
+  console.log(`Language Learning School is coming on port ${port}`);
+});
