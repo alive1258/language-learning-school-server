c8ce80854fa7864b304decb6e14961e5c274fc90
alive1258 <javashazib@gmail.com>
2023-06-09 18:49:37 +0600

    deleted api created for student delete their class

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1yov7jc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,7 +25,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const informationCollection = client.db("languageSchoolDB").collection("informations");
+    const informationCollection = client
+      .db("languageSchoolDB")
+      .collection("informations");
     const cartCollection = client.db("languageSchoolDB").collection("carts");
 
     app.get("/informations", async (req, res) => {
@@ -34,38 +36,37 @@ async function run() {
     });
 
     // class cart collection api
-    app.get('/carts',async(req,res)=>{
-        const email=req.query.email;
-       
-        if(!email){
-            res.send([])
-        }
-        const query ={email:email};
-        const result = await cartCollection.find(query).toArray()
-        res.send(result)
-        
-    })
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/carts", async (req, res) => {
+      // const email = 'coderliton@gmail.com';
+      // const courseItemId = req.body.courseItemId;
 
-        // const email = 'coderliton@gmail.com';     
-        // const courseItemId = req.body.courseItemId;
-        
-        // const query ={email:email,courseItemId:courseItemId};
-        // const scan = await cartCollection.find(query).toArray()
+      // const query ={email:email,courseItemId:courseItemId};
+      // const scan = await cartCollection.find(query).toArray()
 
-        // console.log('scan: '+scan.length)
+      // console.log('scan: '+scan.length)
 
-        const item = req.body;
-        const result = await cartCollection.insertOne(item);
-        res.send(result);
-      });
-    // app.post('/carts',async(req,res)=>{
-    //     const item=req.body;
-    //     console.log(item)
-    //     const result=await CartCollection.insertOne(item)
-    //     res.send(result);
-    // })
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+    // student delete  class
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
